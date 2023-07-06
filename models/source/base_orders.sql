a5dfe9de97d83849adf16ca44fd910d0b55d2eaf
pouria@ifiveo.com <pouria@ifiveo.com>
2023-07-06 23:59:05 +0000

    hi

--- models/source/base_orders.sql
+++ models/source/base_orders.sql
@@ -1,6 +1,9 @@
+ 
+ 
+ 
  {{ config(materialized='view') }} SELECT
  order_id,     
  customer_id,     
- CAST(order_purchase_timestamp AS DATE) AS order_purchase_date 
+order_purchase_timestamp 
  FROM   
 {{ source('public','olist_orders') }}
--- models/source/base_products.sql
+++ models/source/base_products.sql
@@ -1,7 +1,9 @@
--- base_products.sql
+---This query creates a materialized view called "base_products" that retrieves the product ID and product category name 
+---from the "olist_products" table. 
+---It helps in categorizing and analyzing products.
 {{ config(materialized='view') }}
 SELECT
     product_id,
     product_category_name
 FROM
-   {{ source('public',' olist_products') }}
+   {{ source('public','olist_products') }}
